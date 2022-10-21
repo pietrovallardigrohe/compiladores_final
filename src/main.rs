0a3c94f99f9acd79ae65a418b6c36729989daad4
Pietro Vallardi <pietrovallardigrohe@gmail.com>
2022-10-21 17:36:28 -0300

    Main added

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,6 @@ use pest::{Parser, iterators::{Pairs}, error::Error};
 #[grammar = "re.pest"]
 pub struct Re;
 
-
 fn main() {
 
     let test_string: &str = "(16) 28098500;(051)123456789; 1a;ABC-1234;CBA4321; 049 22337059; 049.223.370-59;   0.12 ; 12,20; 1.1; 0,0; <html> <a> </a> b </html>; a; abads; 12.345.678/0001-91;12.12;12345678901234;_as; a";
