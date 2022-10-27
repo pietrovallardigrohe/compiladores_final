097af3137ada14efb303d5d001788f6823cc2c5b
Pietro Vallardi <pietrovallardigrohe@gmail.com>
2022-10-27 17:28:08 -0300

    Grammar done v1

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,56 @@
+use std::fs;
+
 extern crate pest;
 #[macro_use]
 extern crate pest_derive;
 
-use pest::{Parser, iterators::{Pairs}, error::Error};
+use pest::{Parser, error::Error};
+
 #[derive(Parser)]
-#[grammar = "re.pest"]
-pub struct Re;
+#[grammar = "Grammar/e1.pest"]
+pub struct CompilatorParser;
 
-fn main() {
+fn main() -> Result<(), Error<Rule>> {
 
-    let test_string: &str = "(16) 28098500;(051)123456789; 1a;ABC-1234;CBA4321; 049 22337059; 049.223.370-59;   0.12 ; 12,20; 1.1; 0,0; <html> <a> </a> b </html>; a; abads; 12.345.678/0001-91;12.12;12345678901234;_as; a";
+    let file = fs::read_to_string(r"src\target.txt").expect("unable to open file");
+    
+    // file.lines().filter(|e| !e.is_empty()).for_each(|s| print!("l: {}\n", s));
+    for (index, line) in file.lines().enumerate().filter(|(_, e)| !e.is_empty()) {
 
-    for (index, value) in test_string.split(';').into_iter().enumerate() {
+        let parsed_line = CompilatorParser::parse(Rule::Token, line);
+        match parsed_line {
 
-        let pairs: Result<Pairs<Rule>, Error<Rule>> = Re::parse(Rule::identifiers, value);
-
-        if let Ok(p) = pairs {
-            for pair in p.flatten() {
-                match pair.as_rule() {
-                    // Rule::ddd => println!("DDD: {}", pair.as_str()),
-                    Rule::cellphone => println!("Celular: {}", pair.as_str()),
-                    Rule::car_plate => println!("Placa de carro: {}", pair.as_str()),
-                    Rule::cpf => println!("CPF: {}", pair.as_str()),
-                    Rule::real_number => println!("Número Real: {}", pair.as_str()),
-                    Rule::html_tag => println!("Tag HTML: {:?}", pair.as_str()),
-                    Rule::portuguese_word => println!("Palavra: {}", pair.as_str()),
-                    Rule::cnpj => println!("CNPJ: {}", pair.as_str()),
-                    Rule::c_identifier => println!("Identificador C: {}", pair.as_str()),
-                    _ => println!("Não reconhecido")
+            Ok(pairs) => {
+                for pair in pairs {
+                    match pair.as_rule() {
+                        Rule::Variable  
+                        | Rule::Identifier
+                        | Rule::Type 
+                        | Rule::Char 
+                        | Rule::Int 
+                        | Rule::Float => println!("{} - {:?}: \"{}\"", index+1, pair.as_rule(), pair.as_str()),
+                        _ => println!("Não reconhecido \"{}\" {:?} line: {}", pair.as_str(), pair.as_rule(), index)
+                    }
                 }
-            }
-        } else {
-            println!("ERROR PARSING VALUE \"{}\" AT {}", value, index);
+            },
+            Err(error) => println!("Erro linha: {}, ---> \"{}\"", index+1, error.line().to_owned())
+
         }
 
     }
+
+    // println!("{:?}", file.clone());
+    // for pair in file.flatten() {
+    //     match pair.as_rule() {
+    //         // Rule::ddd => println!("DDD: {}", pair.as_str()),
+    //         Rule::tokens => println!("Token: {}", pair.as_str()),
+    //         Rule::variable => println!("Variável: {}", pair.as_str()),
+    //         Rule::var_type => println!("Tipo: {}", pair.as_str()),
+    //         Rule::identifier => println!("Identificador: {}", pair.as_str()),
+    //         _ => println!("Não reconhecido {}", pair.as_str())
+    //     }
+    // }
+
+    Ok(())
 
 }
