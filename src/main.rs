301177471ca876eb926e9eaf042982341f7aa994
Pietro Vallardi <pietrovallardigrohe@gmail.com>
2022-12-09 15:21:05 -0300

    Writing to file modified to single use of writeln!

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 /*
 * Pietro Vallardi Grohe
 * Compiladores
-* Etapa 3
-* 25/11/2022
+* Etapa 4
+* 09/12/2022
 * 
 * Dentro do cmd utilize dentro da pasta compiladores_final
 * "cargo build" para compilar e "cargo run" para rodar o programa 
@@ -26,18 +26,20 @@ use crate::lex::get_tokens;
 use crate::syntax::{syntax_test};
 
 fn main() {
-    let mut out_file = fs::File::create("out.txt").expect("Could not create output file!");
+    // Arquivo de saida, Cria ou apaga o conteúdo
+    let Ok(mut out_file) = fs::File::create("out\\out.txt") else {
+        return println!("Could not create output file!");
+    };
 
-    // std::env::set_var("RUST_BACKTRACE", "1");
+    // Arquivo utilizado em testes
+    let file_path = String::from("src\\tests.txt"); 
 
-    // Lê o caminho do arquivo
+    // Lê o caminho do arquivo de entrada
     // println!("File Path");
     // let mut file_path = String::new(); 
     // std::io::stdin().lock().read_line(&mut file_path).expect("CANNOT READ INPUT");
     // println!("\n\n");
     
-    // Arquivo utilizado em testes
-    let file_path = String::from("src\\tests.txt"); 
     
     // Tenta ler o arquivo para a variável 'file'
     let Ok(raw_input) = fs::read_to_string(Path::new(&file_path.trim_end())) else {
@@ -48,42 +50,45 @@ fn main() {
     input.push_str("EndOfInput");
 
     // Análise léxica
-    let (tokens, errors) = get_tokens(&input);
+    let (tokens, lexical_errors) = get_tokens(&input);
 
     // Printa a análise léxica no arquivo de saida: "out.txt"
-    writeln!(&mut out_file, "Lexical Analyzis==================").unwrap();
-    writeln!(&mut out_file, "\nTOKENS------------------------").unwrap();
-    tokens.iter().for_each(|_token| writeln!(&mut out_file, "{:?}", _token).unwrap());
+
+    let mut output_string = String::new();
+
+    output_string.push_str("Lexical Analyzis==================\n");
+    output_string.push_str("\nTOKENS------------------------\n");
+    tokens.iter().for_each(|_token| output_string.push_str(&format!("{:?}\n", _token)));
 
     // Printa os errors léxicos
-    writeln!(&mut out_file, "\nLexical ERRORS------------------------").unwrap();
-    errors.iter().for_each(|_token| writeln!(&mut out_file, "{:?}", _token).unwrap());
+    output_string.push_str("\nLexical Errors------------------------\n");
+    lexical_errors.iter().for_each(|_token| output_string.push_str(&format!("{:?}\n", _token)));
 
     // Análise Sintática
     let syntax_errors = syntax_test(&tokens);
 
     // Printa a análise sintática no arquivo de saida: "out.txt"
-    writeln!(&mut out_file, "\nSyntactical Analyzis==================").unwrap();
-    writeln!(&out_file, "Syntax ERRORS------------------------").unwrap();
-    syntax_errors.iter().for_each(|err| writeln!(&out_file, "Token: {:?} Message: {}", err.token, err.message).unwrap());
+    output_string.push_str("\nSyntactical Analyzis==================\n");
+    output_string.push_str("Syntax ERRORS------------------------\n");
+    syntax_errors.iter().for_each(|err| output_string.push_str(&format!("Token:{:?} Message: {}\n", err.token, err.message)));
     
-    writeln!(&out_file, "\nOutput: ").unwrap();
-    let mut filtered_input = String::new();
+    output_string.push_str("\nOutput:\n");
     // Se for válido a análize printa o arquivo indicando onde estão os erros
     for (index, line) in raw_input.lines().enumerate() {
-        filtered_input.push_str(&format!("{line}"));
+        output_string.push_str(&format!("{line}"));
         for error in &syntax_errors {
             if error.token.line == index + 1 {
-                filtered_input.push_str(&format!(" <--- Syntax Error || Message: {}", error.message));
+                output_string.push_str(&format!(" <--- Syntax Error || Message: {}", error.message));
                 // continue 'line_iter;
             }
         }
-        filtered_input.push_str("\n");
+        output_string.push_str("\n");
     }
-    writeln!(&out_file, "{}", filtered_input).unwrap();
+
+    writeln!(&mut out_file, "{output_string}").expect("CANNOT WRITE TO FILE");
 
     if syntax_errors.len() == 0 {
-        writeln!(&out_file, "\nNO ERRORS FOUND").unwrap();
+        output_string.push_str("\nNO ERRORS FOUND");
     }
 
     println!("DONE!");
